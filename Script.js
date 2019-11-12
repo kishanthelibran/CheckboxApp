ad31ff5f13fdea4de20be36f7231f712e8481e0e
kishanthelibran <38181487+kishanthelibran@users.noreply.github.com>
2019-11-12 11:33:33 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,26 @@
+﻿function myFunction() {
+    var myid = document.getElementById("myid");
+    var name = document.getElementById("inp1");
+    var br = document.createElement("br");
+    var chkbox = document.createElement("INPUT");
+    chkbox.setAttribute("type", "checkbox");
+    chkbox.checked = true;
+    myid.appendChild(chkbox);
+    var pTag = document.createElement("span");
+    pTag.innerHTML = name.value;
+    myid.appendChild(pTag);
+    var button = document.createElement("button");
+    button.innerHTML = "X";
+    myid.appendChild(button);
+    button.addEventListener("click", function () {
+        if (chkbox.checked)
+            DeleteFunct(chkbox, button, pTag);
+    });
+    myid.appendChild(br);
+}
+function DeleteFunct(chkbox, btn, pTag) {
+    alert("it will be deleted");
+    chkbox.parentElement.removeChild(chkbox);
+    btn.parentElement.removeChild(btn);
+    pTag.parentElement.removeChild(pTag);
+}
